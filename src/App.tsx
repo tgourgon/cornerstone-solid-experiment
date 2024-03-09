e3cfe5d8d136576903561ba7c8514065194d1520
Thomas G <31779479+tgourgon@users.noreply.github.com>
2024-03-09 16:56:37 -0500

    Fixed all Typescript errors

--- src/App.tsx
+++ src/App.tsx
@@ -12,69 +12,37 @@ import {
 
 import {
   IImage,
-  ImageLoaderFn,
   PublicViewportInput
 } from '@cornerstonejs/core/dist/types/types';
 
-//16bit unsigned  little endian
-//width 2560 Height 3272
+// The sample image (.aof) files are raw 16bit unsigned little endian encoded images
+// with width=2560 and Height=3272
 const rows = 3072;
 const colums = 2560;
 
 // Add hardcoded meta data provider for color images
-function hardcodedMetaDataProvider(type: string, imageId: string, imageIds: string) {
+function hardcodedMetaDataProvider(type: string, imageId: string) {
   const colonIndex = imageId.indexOf(':');
   const scheme = imageId.substring(0, colonIndex);
   if (scheme !== 'custom1') {
     return;
   }
 
+  console.log(`Getting Metadata ${type} for ${imageId}`);
   if (type === 'imagePixelModule') {
     const imagePixelModule = {
-      pixelRepresentation: 0,
+      pixelRepresentation: 0, // 0 for unsigned 
       bitsAllocated: 16,
       bitsStored: 16,
       highBit: 16,
-      photometricInterpretation: 'Monochrome1',
+      photometricInterpretation: "MONOCHROME1",
       samplesPerPixel: 1,
     };
 
     return imagePixelModule;
-  } else if (type === 'generalSeriesModule') {
-    const generalSeriesModule = {
-      modality: 'DR',
-      seriesNumber: 1,
-      seriesDescription: 'n/a',
-      seriesDate: '20190201',
-      seriesTime: '120000',
-      seriesInstanceUID: '1.2.276.0.7230010.3.1.4.83233.20190201120000.1',
-    };
-
-    return generalSeriesModule;
-  } else if (type === 'imagePlaneModule') {
-    const index = imageIds.indexOf(imageId);
-    // console.warn(index);
-    const imagePlaneModule = {
-      imageOrientationPatient: [1, 0, 0, 0, 1, 0],
-      imagePositionPatient: [0, 0, index * 5],
-      pixelSpacing: [1, 1],
-      columnPixelSpacing: 1,
-      rowPixelSpacing: 1,
-      frameOfReferenceUID: 'FORUID',
-      columns: colums,
-      rows: rows,
-      rowCosines: [1, 0, 0],
-      columnCosines: [0, 1, 0],
-    };
-
-    return imagePlaneModule;
   } else if (type === 'voiLutModule') {
     return {
-      // According to the DICOM standard, the width is the number of samples
-      // in the input, so 256 samples.
       windowWidth: [65535],
-      // The center is offset by 0.5 to allow for an integer value for even
-      // sample counts
       windowCenter: [65535 / 2],
     };
   } else if (type === 'modalityLutModule') {
@@ -87,26 +55,22 @@ function hardcodedMetaDataProvider(type: string, imageId: string, imageIds: stri
   }
 }
 
-function loadImage(imageId: string): Promise<ImageLoaderFn> {
-  // Parse the imageId and return a usable URL (logic omitted)
-  // const url = parseImageId(imageId);
+function loadImage(imageId: string): { promise: Promise<IImage> } {
+
+  //TODO Parse ImageId to extrack URL
   const url = "http://localhost:3000/samples/sample_1.aof";
 
   // Create a new Promise
-  const promise = new Promise((resolve, reject) => {
-    // Inside the Promise Constructor, make
-    // the request for the DICOM data
+  const promise = new Promise<IImage>((resolve, reject) => {
     const oReq = new XMLHttpRequest();
     oReq.open('get', url, true);
     oReq.responseType = 'arraybuffer';
     oReq.onreadystatechange = function (oEvent) {
       if (oReq.readyState === 4) {
         if (oReq.status == 200) {
-          // Request succeeded, Create an image object (logic omitted)
-          //const image = createImageObject(oReq.response);
           console.log(oReq);
           const image: IImage = {
-            imageId: "sample",
+            imageId: imageId,
             minPixelValue: 0,
             maxPixelValue: 65535,
             windowCenter: 65535 / 2,
@@ -126,8 +90,10 @@ function loadImage(imageId: string): Promise<ImageLoaderFn> {
             width: colums,
             getPixelData: () => { return oReq.response },
             getCanvas: () => document.createElement("canvas"),
-            voiLUTFunction: ""
+            voiLUTFunction: Enums.VOILUTFunctionType.LINEAR
           };
+          console.log("Returning image object");
+          console.log(image);
           // Return the image object by resolving the Promise
           resolve(image);
         } else {
@@ -143,7 +109,7 @@ function loadImage(imageId: string): Promise<ImageLoaderFn> {
 
   // Return an object containing the Promise to cornerstone so it can setup callbacks to be
   // invoked asynchronously for the success/resolve and failure/reject scenarios.
-  return({
+  return ({
     promise,
   });
 }
@@ -157,14 +123,15 @@ const App: Component = () => {
     imageLoader.registerImageLoader('custom1', loadImage);
 
     metaData.addProvider(
-      // @ts-ignore
-      (type, imageId) => hardcodedMetaDataProvider(type, imageId, imageIds),
+      (type, imageId) => hardcodedMetaDataProvider(type, imageId),
       10000
     );
 
 
-    csDiv.style.width = `${colums}}px`;
-    csDiv.style.height = `${rows}px`;
+    // csDiv.style.width = `${colums}}px`;
+    // csDiv.style.height = `${rows}px`;
+    csDiv.style.width = `500px`;
+    csDiv.style.height = `500px`;
 
     const renderingEngineId = 'myRenderingEngine';
     const renderingEngine = new RenderingEngine(renderingEngineId);
@@ -188,7 +155,7 @@ const App: Component = () => {
 
   return (
     <>
-      <h1> Hello cornerstone</h1>
+      <h1>Cornerstone3D Raw Image Viewer</h1>
       <div ref={csDiv} id='content'></div>
     </>
   );
